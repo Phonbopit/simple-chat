2f2d2d80ee198f3bebf176d948b72f4564ca3a46
Chai Phonbopit <phonbopit@gmail.com>
2015-02-12 22:04:29 +0700

    fixed path.join()

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 // ตั้งค่า เพื่อให้ express ทำการ render view ที่โฟลเดอร์ views
 // และใช้ template engine เป็น jade
-app.set('views', path.join(__dirname + '/views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
